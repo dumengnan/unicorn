bd33135abbf5fb1ebebe5f9b352bec126b13b0cd
dumengnan <1107593797@qq.com>
2018-02-24 09:59:26 +0800

    提交airflow 相关

---
+++ 06source_code/tools/unicorn-airflow/dags/unicorn-daily-dag/unicorn_daily_dag.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+from unicorn.airflow.util.unicorn_airflow_util import load_yaml
+
+dag_id = "unicorn_daily_dag"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+dag_config = load_yaml(os.path.join(dir_path, dag_id + ".yml"))
+
+default_args = dag_config['default_args']
+default_args['start_date'] = datetime.now()
+
+dag = DAG(dag_id,
+          default_args=dag_config["default_args"],
+          schedule_interval=dag_config["schedule_interval"])
+
+dag.doc_md = dag_config['doc_md']
+
+t1 = BashOperator(task_id='TaskStart', bash_command="date", dag=dag)
+
+
+
+
+
