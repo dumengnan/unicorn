8c8bd637e03b54bc84b313a4e77f4578e70a48d4
dumengnan <1107593797@qq.com>
2018-03-03 17:45:03 +0800

    format 代码

--- 06source_code/tools/data-importer/src/main/java/com/unicorn/data/service/UnicornDataImporter.java
+++ 06source_code/tools/data-importer/src/main/java/com/unicorn/data/service/UnicornDataImporter.java
@@ -11,66 +11,65 @@ import org.slf4j.LoggerFactory;
 
 public class UnicornDataImporter {
 
-    private static Logger log = LoggerFactory.getLogger(UnicornDataImporter.class);
+  private static Logger log = LoggerFactory.getLogger(UnicornDataImporter.class);
 
 
-    private static void printUsage(Options options) {
-        final HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("UnicornDataImporter", options);
+  private static void printUsage(Options options) {
+    final HelpFormatter formatter = new HelpFormatter();
+    formatter.printHelp("UnicornDataImporter", options);
+  }
+
+  public static Options getOptions() {
+    Option help = new Option("h", "the command help");
+
+    // 此处定义参数类似于 java 命令中的 -D<name>=<value>
+    Option property = OptionBuilder.withArgName("property=value")
+        .hasArgs(2).withValueSeparator().withDescription(
+            "search the objects which have the target property and value").create("D");
+    Options opts = new Options();
+    opts.addOption(help);
+    opts.addOption(property);
+
+    return opts;
+  }
+
+  CommandLine parseCommandLine(String[] args) throws Exception {
+    DefaultParser parser = new DefaultParser();
+
+    return parser.parse(getOptions(), args);
+  }
+
+
+  public void run() throws Exception {
+    SparkConf conf = new SparkConf().setAppName("UnicornDataImporter");
+    JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(3));
+
+    jssc.start();
+    jssc.awaitTermination();
+    jssc.stop();
+
+  }
+
+  public static void doMain(String[] args) {
+    UnicornDataImporter dataImporter = new UnicornDataImporter();
+
+    try {
+      CommandLine line = dataImporter.parseCommandLine(args);
+      if (line.hasOption("help")) {
+        printUsage(getOptions());
+      }
+
+      dataImporter.run();
+    } catch (Exception ex) {
+      log.error(" ", ex);
     }
+  }
 
-    public static Options getOptions() {
-        Option help = new Option("h", "the command help");
-
-        // 此处定义参数类似于 java 命令中的 -D<name>=<value>
-        Option property = OptionBuilder.withArgName("property=value")
-                .hasArgs(2).withValueSeparator().withDescription(
-                        "search the objects which have the target property and value").create("D");
-        Options opts = new Options();
-        opts.addOption(help);
-        opts.addOption(property);
-
-        return opts;
+  public static void main(String[] args) {
+    try {
+      doMain(args);
+    } catch (Exception ex) {
+      log.error("Data Importer Exception ", ex);
     }
-
-    CommandLine parseCommandLine(String[] args) throws Exception {
-        DefaultParser parser = new DefaultParser();
-
-        return parser.parse(getOptions(), args);
-    }
-
-
-    public void run() throws Exception {
-        SparkConf conf = new SparkConf().setAppName("UnicornDataImporter");
-        JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(3));
-
-
-        jssc.start();
-        jssc.awaitTermination();
-        jssc.stop();
-
-    }
-
-    public static void doMain(String[] args) {
-        UnicornDataImporter dataImporter = new UnicornDataImporter();
-
-        try {
-            CommandLine line = dataImporter.parseCommandLine(args);
-            if (line.hasOption("help")) {
-                printUsage(getOptions());
-            }
-
-            dataImporter.run();
-        } catch (Exception ex) {
-            log.error(" ", ex);
-        }
-    }
-
-    public static void main(String[] args) {
-        try {
-            doMain(args);
-        } catch (Exception ex) {
-            log.error("Data Importer Exception ", ex);
-        }
-    }
+  }
 }
--- 06source_code/tools/data-importer/src/test/java/com/unicorn/data/service/UnicornDataImporterTest.java
+++ 06source_code/tools/data-importer/src/test/java/com/unicorn/data/service/UnicornDataImporterTest.java
@@ -9,17 +9,17 @@ import java.util.Properties;
 
 public class UnicornDataImporterTest {
 
-    @Test
-    public void testGetOptions() throws Exception {
-        String[] args = {"-Dkey1=value1", "-Dkey2=value2"};
+  @Test
+  public void testGetOptions() throws Exception {
+    String[] args = {"-Dkey1=value1", "-Dkey2=value2"};
 
-        UnicornDataImporter dataImporter = new UnicornDataImporter();
+    UnicornDataImporter dataImporter = new UnicornDataImporter();
 
-        CommandLine line = dataImporter.parseCommandLine(args);
+    CommandLine line = dataImporter.parseCommandLine(args);
 
-        Properties commandProp = line.getOptionProperties("D");
+    Properties commandProp = line.getOptionProperties("D");
 
-        Assert.assertEquals("value1", commandProp.getProperty("key1"));
-        Assert.assertEquals("value2", commandProp.getProperty("key2"));
-    }
+    Assert.assertEquals("value1", commandProp.getProperty("key1"));
+    Assert.assertEquals("value2", commandProp.getProperty("key2"));
+  }
 }
