e6feea3a7c12efe3871c9f6a7a44cc336bf07bc9
dumengnan <1107593797@qq.com>
2018-03-07 20:49:37 +0800

    添加发送mysql 的步骤

--- 06source_code/tools/data-importer/src/main/java/com/unicorn/data/sender/MysqlSender/MysqlSenderService.java
+++ 06source_code/tools/data-importer/src/main/java/com/unicorn/data/sender/MysqlSender/MysqlSenderService.java
@@ -1,6 +1,8 @@
 package com.unicorn.data.sender.MysqlSender;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -29,23 +31,32 @@ public class MysqlSenderService {
         Connection connection = this.connectionPool.getConnection();
         try {
             connection.setAutoCommit(false);
+            String insertSql = "INSERT INTO social_user (ID,SCREEN_NAME,NAME_STR,CREATED_AT,"
+                    + "FOLLOWERS_COUNT,FRIENDS_COUNT,STATUSES_COUNT,SOCIAL_TYPE,USER_LANG,UPDATE_TIME)"
+                    + " values (?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement stat = connection.prepareStatement(insertSql);
+
             while(recordIterator.hasNext()) {
                 ConsumerRecord<String, String> record = recordIterator.next();
 
                 String topic = record.topic();
                 String lineValue = record.value();
-
+                String[] lineArr = lineValue.split(",");
                 if (sendToMysqlTopic.contains(topic)) {
                     logger.debug("Send Data To Mysql");
+
+                    for (int i = 0; i < lineArr.length; ++i) {
+                        stat.setObject(i + 1, lineArr[i]);
+                    }
                 }
-
-
             }
-
+            stat.executeBatch();
+            connection.commit();
         } catch (Exception ex) {
             logger.error("Send To Mysql Exception :", ex);
+        } finally {
+            connection.close();
         }
-
     }
 
 }
