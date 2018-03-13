4379017c6a00d196cfc2b3d225847d113f640266
dumengnan <1107593797@qq.com>
2018-03-13 14:39:11 +0800

    修改爬取推文的 使其可以直接读取cookie 直接运行

--- 06source_code/crawler/custom/shandong/unicorn/crawler/crawl_guomedia_status.py
+++ 06source_code/crawler/custom/shandong/unicorn/crawler/crawl_guomedia_status.py
@@ -12,15 +12,15 @@ def crawl_status():
     param['get'] = 'posts_profile'
     param['filter'] = 'all'
 
-    offset = 1
-    with open('follwers_list', 'r') as f_input:
+    offset = 0
+    with open('follwers_list.txt', 'r') as f_input:
         with open('status_list.txt', 'w')as f_output:
             for line in f_input:
                 line_arr = line.strip().split("\t")
                 if len(line_arr) < 2:
                     continue
 
-                param['id'] = line_arr[1]
+                param['id'] = line_arr[2]
                 while True:
                     param['offset'] = offset
                     contents = guo_media_util.request_data(param)
@@ -32,14 +32,28 @@ def crawl_status():
 
                     soup = BeautifulSoup(data_content_html, 'lxml')
                     post_list = soup.find_all('div', class_='post')
+                    offset = offset + 1
                     for post in post_list:
-                        data_id = post['data_id']
+                        status_info_list = list()
+                        data_id = post.attrs['data-id']
                         post_time = post.find('div', class_='post-time').find('a')['data-time']
                         data_text = post.find('div', class_='post-text-plain hidden').get_text()
 
                         comments_count = post.find('span', id='span-comments-counter_' + data_id).get_text()
                         share_count = post.find('span', id='span-share-counter_' + data_id).get_text()
                         like_count = post.find('span', class_='span-counter_' + data_id).get_text()
+
+                        status_info_list.append(data_id)
+                        status_info_list.append(post_time)
+                        status_info_list.append(comments_count)
+                        status_info_list.append(share_count)
+                        status_info_list.append(like_count)
+                        status_info_list.append(data_text)
+
+                        print "\t".join(status_info_list)
+                        f_output.write("\t".join(status_info_list) + "\n")
+
+
 
 
 if __name__ == '__main__':
--- 06source_code/crawler/custom/shandong/unicorn/crawler/guo_media_util.py
+++ 06source_code/crawler/custom/shandong/unicorn/crawler/guo_media_util.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import requests
 import time
 
@@ -71,27 +72,53 @@ def get_proxy_dict():
 
 
 def request_data(param):
-    options = webdriver.ChromeOptions()
+    # options = webdriver.ChromeOptions()
+    #
+    # # options.add_argument('--proxy-server=%s' % 'http://192.168.1.3:8119')
+    # # options.add_argument('--user-data-dir=C:/Users/Administrator/AppData/Local/Google/Chrome/User Data/Default')
+    # options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
+    # # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument("window-size=1920*1080")
+    # browser = webdriver.Chrome(chrome_options=options, executable_path='tools/chromedriver.exe')
+    # browser.get(LOGIN_URL)
+    #
+    # username = browser.find_element_by_name('username_email')
+    # password = browser.find_element_by_name('password')
+    #
+    # username.send_keys("demohaha")
+    # password.send_keys("demohaha")
+    #
+    # browser.find_element_by_xpath("//input[@type='submit' and @value='登录']").click()
+    #
+    # cookies = browser.get_cookies()
 
-    # options.add_argument('--proxy-server=%s' % 'http://192.168.1.3:8119')
-    # options.add_argument('--user-data-dir=C:/Users/Administrator/AppData/Local/Google/Chrome/User Data/Default')
-    options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
-    # options.add_argument('--headless')
-    options.add_argument('--disable-gpu')
-    options.add_argument("window-size=1920*1080")
-    browser = webdriver.Chrome(chrome_options=options, executable_path='tools/chromedriver.exe')
-    browser.get(LOGIN_URL)
+    cookies = [
+        {u'domain': u'www.guo.media', u'name': u'PHPSESSID', u'value': u'5cvubd4m9ahnaher1n3c20vp1k', u'path': u'/',
+         u'httpOnly': False, u'secure': False},
+        {u'domain': u'.guo.media', u'secure': True, u'value': u'd7ba8e2d436dfc133accbdfc4682458341520922149',
+         u'expiry': 1552458149.639698, u'path': u'/', u'httpOnly': True, u'name': u'__cfduid'},
+        {u'domain': u'.guo.media', u'name': u'__cfruid',
+         u'value': u'fe7aa52fbfced8f4ff6aa87e4b1dad59188d49ee-1520922149', u'path': u'/', u'httpOnly': True,
+         u'secure': False}, {u'domain': u'.guo.media', u'secure': False, u'value': u'66867e17c783d30ffcae9f582a9ac755',
+                             u'expiry': 1521008599.678981, u'path': u'/', u'httpOnly': False, u'name': u'srv_id'},
+        {u'domain': u'.guo.media', u'secure': False, u'value': u'GA1.2.773077139.1520922165', u'expiry': 1521008596,
+         u'path': u'/', u'httpOnly': False, u'name': u'_gid'},
+        {u'domain': u'.guo.media', u'secure': False, u'value': u'GA1.2.1320845651.1520922165', u'expiry': 1583994196,
+         u'path': u'/', u'httpOnly': False, u'name': u'_ga'},
+        {u'domain': u'.guo.media', u'secure': False, u'value': u'1', u'expiry': 1520922225, u'path': u'/',
+         u'httpOnly': False, u'name': u'_gat_gtag_UA_111414205_1'},
+        {u'domain': u'.guo.media', u'secure': False, u'value': u'c5225f1853dd92a8968978d650d110c4',
+         u'expiry': 1523514172.655182, u'path': u'/', u'httpOnly': False, u'name': u'xs'},
+        {u'domain': u'.guo.media', u'secure': False, u'value': u'49404', u'expiry': 1523514172.655132, u'path': u'/',
+         u'httpOnly': False, u'name': u'c_user'},
+        {u'domain': u'www.guo.media', u'secure': False, u'value': u'83690d5b0979ac2cd030ce86579180b0',
+         u'expiry': 253402300799L, u'path': u'/', u'httpOnly': False, u'name': u'user_id'}]
 
-    username = browser.find_element_by_name('username_email')
-    password = browser.find_element_by_name('password')
-
-    username.send_keys("demohaha")
-    password.send_keys("demohaha")
-
-    browser.find_element_by_xpath("//input[@type='submit' and @value='登录']").click()
-
+    print cookies
     s = requests.Session()
-    for cookie in browser.get_cookies():
+    for cookie in cookies:
+        print cookie['name'], cookie['value']
         s.cookies.set(cookie['name'], cookie['value'])
 
     contents = s.post(LOAD_DATA_URL, headers=get_headers(), data=param, proxies=get_proxy_dict())
