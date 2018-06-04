4571484928567064167f566185826c9844a6b483
dumengnan <1107593797@qq.com>
2018-06-04 19:31:57 +0800

    修改shell 脚本

--- 06source_code/tools/data-importer/bin/data_importer.sh
+++ 06source_code/tools/data-importer/bin/data_importer.sh
@@ -11,42 +11,13 @@ done
 current_dir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 MYAPPLICATION_HOME="$current_dir"/..
 
-if [ "x$MYAPPLICATION_INCLUDE" = "x" ]; then
-    for include in  /opt/unicorn-tools/data-importer/data_importer.in.sh \
-                   $MYAPPLICATION_HOME/bin/data_importer.in.sh; do
-        if [ -r $include ]; then
-            . $include
-            break
-        fi
-    done
-elif [ -r $MYAPPLICATION_INCLUDE ]; then
-    . $MYAPPLICATION_INCLUDE
-fi
+set -x 
 
-if [ -x "$JAVA_HOME/bin/java" ]; then
-    JAVA="$JAVA_HOME/bin/java"
-else
-    JAVA=`which java`
-fi
-
-if [ ! -x "$JAVA" ]; then
-    echo "Could not find any executable java binary. Please install java in your PATH or set JAVA_HOME"
-    exit 1
-fi
-
-if [ -z $MYAPPLICATION_CLASSPATH ]; then
-    echo "You must set the  CLASSPATH vars" >&2
-    exit 1
-fi
-
-# Special-case path variables.
-case "`uname`" in
-    CYGWIN*) 
-        MYAPPLICATION_CLASSPATH=`cygpath -p -w "$MYAPPLICATION_CLASSPATH"`
-        MYAPPLICATION_CONF=`cygpath -p -w "$MYAPPLICATION_CONF"`
-    ;;
-esac
-
-
-
+spark-submit \
+  --class com.unicorn.data.service.UnicornDataImporter \
+  --master local[*] \
+  $MYAPPLICATION_HOME/lib/*.jar \
+  --conf spark.executor.extraClassPath=$MYAPPLICATION_HOME \
+  --files $MYAPPLICATION_HOME/etc/config.properties,$MYAPPLICATION_HOME/etc/consumer.properties
+  
 exit $?
