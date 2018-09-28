55be337b229437ee615d1c4c87792ef16736f826
dumengnan <1107593797@qq.com>
2018-09-28 16:50:38 +0800

    修改认证服务  把config 相关独立出去

--- 06source_code/service-center/auth-service/src/main/java/com/sz/auth/AuthApplication.java
+++ 06source_code/service-center/auth-service/src/main/java/com/sz/auth/AuthApplication.java
@@ -1,7 +1,5 @@
 package com.sz.auth;
 
-
-import com.sz.auth.service.security.MysqlUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
@@ -35,95 +33,5 @@ public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
-	}
-
-	@Configuration
-	@EnableWebSecurity
-	protected static class webSecurityConfig extends WebSecurityConfigurerAdapter {
-
-		@Autowired
-		private MysqlUserDetailsService userDetailsService;
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			// @formatter:off
-			http
-					.authorizeRequests().anyRequest().authenticated()
-					.and()
-					.csrf().disable();
-			// @formatter:on
-		}
-
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			auth.userDetailsService(userDetailsService)
-					.passwordEncoder(new BCryptPasswordEncoder());
-		}
-
-		@Override
-		@Bean
-		public AuthenticationManager authenticationManagerBean() throws Exception {
-			return super.authenticationManagerBean();
-		}
-	}
-
-	@Configuration
-	@EnableAuthorizationServer
-	protected static class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
-
-		private TokenStore tokenStore = new InMemoryTokenStore();
-
-		@Autowired
-		@Qualifier("authenticationManagerBean")
-		private AuthenticationManager authenticationManager;
-
-		@Autowired
-		private MysqlUserDetailsService userDetailsService;
-
-		@Autowired
-		private Environment env;
-
-		@Override
-		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-
-			// TODO persist clients details
-
-			// @formatter:off
-			clients.inMemory()
-					.withClient("browser")
-					.authorizedGrantTypes("refresh_token", "password")
-					.scopes("ui")
-					.and()
-					.withClient("account-service")
-					.secret(env.getProperty("ACCOUNT_SERVICE_PASSWORD"))
-					.authorizedGrantTypes("client_credentials", "refresh_token")
-					.scopes("server")
-					.and()
-					.withClient("statistics-service")
-					.secret(env.getProperty("STATISTICS_SERVICE_PASSWORD"))
-					.authorizedGrantTypes("client_credentials", "refresh_token")
-					.scopes("server")
-					.and()
-					.withClient("notification-service")
-					.secret(env.getProperty("NOTIFICATION_SERVICE_PASSWORD"))
-					.authorizedGrantTypes("client_credentials", "refresh_token")
-					.scopes("server");
-			// @formatter:on
-		}
-
-		@Override
-		public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-			endpoints
-					.tokenStore(tokenStore)
-					.authenticationManager(authenticationManager)
-					.userDetailsService(userDetailsService);
-		}
-
-		@Override
-		public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-			oauthServer
-					.tokenKeyAccess("permitAll()")
-					.checkTokenAccess("isAuthenticated()");
-		}
 	}
 }
