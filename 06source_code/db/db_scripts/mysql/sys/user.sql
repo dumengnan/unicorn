3209f1bf3ee3920f6c1d9c29d78f2ac0d2d6bb46
dumengnan <1107593797@qq.com>
2018-09-29 17:40:00 +0800

    修改表结构 添加字段

--- 06source_code/db/db_scripts/mysql/sys/user.sql
+++ 06source_code/db/db_scripts/mysql/sys/user.sql
@@ -10,6 +10,7 @@ create table oauth_client_details (
     client_id VARCHAR(256) PRIMARY KEY,
     resource_ids VARCHAR(256),
     client_secret VARCHAR(256),
+    secret_required VARCHAR(256),
     scope VARCHAR(256),
     authorized_grant_types VARCHAR(256),
     web_server_redirect_uri VARCHAR(256),
@@ -17,13 +18,13 @@ create table oauth_client_details (
     access_token_validity INTEGER,
     refresh_token_validity INTEGER,
     additional_information VARCHAR(4096),
-    autoapprove VARCHAR(256)
+    auto_approve VARCHAR(256)
 );
 
 INSERT INTO oauth_client_details
-    (client_id, client_secret, scope, authorized_grant_types,
+    (client_id, resource_ids ,secret_required, scope, authorized_grant_types,
     web_server_redirect_uri, authorities, access_token_validity,
-    refresh_token_validity, additional_information, autoapprove)
+    refresh_token_validity, additional_information, auto_approve)
 VALUES
-    ("browser", null, "ui",
+    ("browser", "browser", null, false, "ui",
     "password,refresh_token", null, null, 36000, 36000, null, true);
