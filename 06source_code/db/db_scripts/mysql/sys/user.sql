94ada054be987d2691f5c7a40d471ccdb173bf72
dumengnan <1107593797@qq.com>
2018-09-30 10:21:25 +0800

    修改建表脚本  添加自增主键

--- 06source_code/db/db_scripts/mysql/sys/user.sql
+++ 06source_code/db/db_scripts/mysql/sys/user.sql
@@ -1,8 +1,8 @@
 CREATE TABLE user(
-    id     INTEGER, 
-	username varchar(100) NOT NULL PRIMARY KEY,
+    id int(11) NOT NULL PRIMARY KEY auto_increment, 
+	username varchar(100) ,
 	password varchar(100), 
-	remark varchar(100)
+	remark varchar(100),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
