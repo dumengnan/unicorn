12b3856108f2e0542e67b21f9879725410f2f4a8
dumengnan <1107593797@qq.com>
2018-12-30 13:00:45 +0800

    语音识别 1、修改单元测试不能正常运行的问题 nose 不支持目录嵌套

---
+++ 06source_code/service-center/language-service/test/unit/test_detect_language.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+'''
+Created on 2018
+
+@author: Administrator
+'''
+import unittest
+from langdetect import detect,DetectorFactory
+
+    
+
+class Test(unittest.TestCase):
+    
+    def testDetectLanguage(self):
+        DetectorFactory.seed = 0
+        lang = detect(u"这是一个最好的世界")
+        self.assertEqual("zh-cn", lang)
+        
+        ja_lang = detect(u"今一はお前さん")
+        self.assertEqual("ja", ja_lang)
+        
+        ar_lang = detect(u"العَرَبِيَّة‎")
+        self.assertEqual("ar", ar_lang)
+        
+        ar_lang2 = detect(u"ئئۇيغۇر كومپيۇتېر يېزىقى")
+        self.assertEqual("ar", ar_lang2)
+        
+        
+
+if __name__ == "__main__":
+    unittest.main()
