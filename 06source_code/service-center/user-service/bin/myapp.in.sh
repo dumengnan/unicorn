29e2b168c1a04959b64dd8abb23e97afdea2707e
dumengnan <1107593797@qq.com>
2019-06-01 22:00:46 +0800

    添加user service

---
+++ 06source_code/service-center/user-service/bin/myapp.in.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+# check in case a user was using this mechanism
+if [ "x$MYAPPLICATION_CLASSPATH" != "x" ]; then
+    cat >&2 << EOF
+Error: Don't modify the classpath with MYAPPLICATION_CLASSPATH.
+Add plugins and their dependencies into the plugins/ folder instead.
+EOF
+    exit 1
+fi
+MYAPPLICATION_CLASSPATH=$MYAPPLICATION_HOME/lib/user-service.jar:$MYAPPLICATION_HOME/etc
+if [ "x$MYAPPLICATION_MIN_MEM" = "x" ]; then
+    MYAPPLICATION_MIN_MEM=256m
+fi
+if [ "x$MYAPPLICATION_HEAP_SIZE" != "x" ]; then
+    MYAPPLICATION_MIN_MEM=$MYAPPLICATION_HEAP_SIZE
+    MYAPPLICATION_MAX_MEM=$MYAPPLICATION_HEAP_SIZE
+fi
+# min and max heap sizes should be set to the same value to avoid
+# stop-the-world GC pauses during resize, and so that we can lock the
+# heap in memory on startup to prevent any of it from being swapped
+# out.
+JAVA_OPTS="$JAVA_OPTS -Xms${MYAPPLICATION_MIN_MEM}"
+if [ "x$MYAPPLICATION_MAX_MEM" != "x" ]; then
+    JAVA_OPTS="$JAVA_OPTS -Xmx${MYAPPLICATION_MAX_MEM}"
+fi
+# new generation
+if [ "x$MYAPPLICATION_HEAP_NEWSIZE" != "x" ]; then
+    JAVA_OPTS="$JAVA_OPTS -Xmn${MYAPPLICATION_HEAP_NEWSIZE}"
+fi
+# max direct memory
+if [ "x$MYAPPLICATION_DIRECT_SIZE" != "x" ]; then
+    JAVA_OPTS="$JAVA_OPTS -XX:MaxDirectMemorySize=${MYAPPLICATION_DIRECT_SIZE}"
+fi
+# set to headless, just in case
+JAVA_OPTS="$JAVA_OPTS -Djava.awt.headless=true"
+# Force the JVM to use IPv4 stack
+if [ "x$MYAPPLICATION_USE_IPV4" != "x" ]; then
+  JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true"
+fi
+JAVA_OPTS="$JAVA_OPTS -XX:+UseParNewGC"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseConcMarkSweepGC"
+JAVA_OPTS="$JAVA_OPTS -XX:CMSInitiatingOccupancyFraction=75"
+JAVA_OPTS="$JAVA_OPTS -XX:+UseCMSInitiatingOccupancyOnly"
+# GC logging options
+if [ "x$MYAPPLICATION_USE_GC_LOGGING" != "x" ]; then
+  JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDetails"
+  JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCTimeStamps"
+  JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCDateStamps"
+  JAVA_OPTS="$JAVA_OPTS -XX:+PrintClassHistogram"
+  JAVA_OPTS="$JAVA_OPTS -XX:+PrintTenuringDistribution"
+  JAVA_OPTS="$JAVA_OPTS -XX:+PrintGCApplicationStoppedTime"
+  GC_LOG_DIR="$MYAPPLICATION_HOME/logs";
+  JAVA_OPTS="$JAVA_OPTS -Xloggc:$GC_LOG_DIR/gc.log"
+  # Ensure that the directory for the log file exists: the JVM will not create it.
+  if [[ ! -d "$GC_LOG_DIR" ||  ! -x "$GC_LOG_DIR" ]]; then
+    cat >&2 << EOF
+Error: GC log directory '$GC_LOG_DIR' does not exist or is not accessible.
+EOF
+    exit 1
+  fi
+fi
+# Disables explicit GC
+JAVA_OPTS="$JAVA_OPTS -XX:+DisableExplicitGC"
+# Ensure UTF-8 encoding by default (e.g. filenames)
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8"
+# Use our provided JNA always versus the system one
+JAVA_OPTS="$JAVA_OPTS -Djna.nosys=true"
+# log4j options
+JAVA_OPTS="$JAVA_OPTS -Dlog4j.shutdownHookEnabled=false -Dlog4j2.disable.jmx=true -Dlog4j.skipJansi=true"
---
+++ 06source_code/service-center/user-service/bin/myapp.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  current_dir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$current_dir/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+# 获取当前工作目录
+current_dir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+MYAPPLICATION_HOME="$current_dir"/..
+
+if [ "x$MYAPPLICATION_INCLUDE" = "x" ]; then
+    for include in $MYAPPLICATION_HOME/bin/myapp.in.sh; do
+        if [ -r $include ]; then
+            . $include
+            break
+        fi
+    done
+elif [ -r $MYAPPLICATION_INCLUDE ]; then
+    . $MYAPPLICATION_INCLUDE
+fi
+
+if [ -x "$JAVA_HOME/bin/java" ]; then
+    JAVA="$JAVA_HOME/bin/java"
+else
+    JAVA=`which java`
+fi
+
+if [ ! -x "$JAVA" ]; then
+    echo "Could not find any executable java binary. Please install java in your PATH or set JAVA_HOME"
+    exit 1
+fi
+
+if [ -z $MYAPPLICATION_CLASSPATH ]; then
+    echo "You must set the  CLASSPATH vars" >&2
+    exit 1
+fi
+
+# Special-case path variables.
+case "`uname`" in
+    CYGWIN*) 
+        MYAPPLICATION_CLASSPATH=`cygpath -p -w "$MYAPPLICATION_CLASSPATH"`
+        MYAPPLICATION_CONF=`cygpath -p -w "$MYAPPLICATION_CONF"`
+    ;;
+esac
+
+# 默认后台启动
+# The option will tell MYAPPLICATIONDaemon not
+# to close stdout/stderr, but it's up to us not to background.
+daemonized=false
+if [ "x$daemonized" != "x" ]; then
+    exec $JAVA $JAVA_OPTS  -cp $MYAPPLICATION_CLASSPATH -DrootDir=$MYAPPLICATION_HOME \
+            org.springframework.boot.loader.JarLauncher  "$@"
+# Startup piggy config , background it
+else
+    exec $JAVA $JAVA_OPTS  -cp $MYAPPLICATION_CLASSPATH -DrootDir=$MYAPPLICATION_HOME \
+                org.springframework.boot.loader.JarLauncher "$@" <&- &
+fi
+
+
+exit $?
